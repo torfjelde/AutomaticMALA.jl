c66366bf5f6382f4c0a2339a46d050ac8a12ca0e
Documenter.jl <documenter@juliadocs.github.io>
2024-05-16 17:57:40 +0000

    build based on 3e333f7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AutomaticMALA","category":"page"},{"location":"#AutomaticMALA","page":"Home","title":"AutomaticMALA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AutomaticMALA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AutomaticMALA]","category":"page"}]
+}
